b4048d1d3ffac4ce14de0dd7fe03dd64f0d08b70
Kharim234 <malinowskimichal27@gmail.com>
2017-11-13 19:44:27 +0100

    commit

---
+++ lab1/test/ConsoleApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+using System;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            byte m;
+            byte b = 0;
+            byte a = 0;
+            char[] zmienne = new char[] {'d', 'c', 'x'};
+            foreach (var v in zmienne)
+            {
+                for (int j = 1; j < 8; j++)
+                {
+                    m = (byte) (1 << (j - 1));
+
+                    b = (byte) ((v & m) >> (j - 1));
+                    a ^= b;
+                }
+            }
+            
+           Console.WriteLine(a);
+            }
+
+        }
+    }
---
+++ lab1/test/test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test
+{
+    public class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world!!!");
+        }
+    }
+}
