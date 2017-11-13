57077e5b3c66e954e0f53896e401d27eb67e2e21
Kharim234 <malinowskimichal27@gmail.com>
2017-11-13 19:22:33 +0100

    Suma Kontrolna

---
+++ lab1/SumaKontrolna/SumaKontrolna/Suma_kontrolna.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumaKontrolna
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte m;
+            byte b = 0;
+            byte a = 0;
+            char[] zmienne = new char[] {'d', 'c', 'x'};
+            foreach (var v in zmienne)
+            {
+                for (int j = 1; j < 8; j++)
+                {
+                    m = (byte) (1 << (j - 1));
+
+                    b = (byte) ((v & m) >> (j - 1));
+                    a ^= b;
+                }
+            }
+            
+            Console.WriteLine(a);
+        }
+    }
+}
